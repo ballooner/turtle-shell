272c614abfec02946d787c4e92032b4d59eba274
Home Desktop-WSL Ubuntu <26182964+ballooner@users.noreply.github.com>
2025-06-26 08:57:04 -0400

    Updated todo

--- include/Parser.h
+++ include/Parser.h
@@ -1,6 +1,15 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <stdbool.h>
 
+typedef struct Command 
+{
+	char* name;
+	char** flags;
+	char** args;
+	bool output_redir;
+	bool input_redir;
+} command_t;
 
 #endif
